7e4f2e726f54791881c56c6758e23e7ed062caf2
Akash Gupta <87561686+aakaash123@users.noreply.github.com>
2022-10-03 00:00:32 +0530

    Add files via upload

---
+++ itemloaders.py
@@ -0,0 +1,7 @@
+#############################
+# Item Loaders
+# Mainly called by spiders to define rules to load items defined in items.py
+# Author: (EJ) Vivek Pandey
+# Last Modified: 03/05/2019
+#############################
+
